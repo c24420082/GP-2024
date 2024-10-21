4700f3fce09fcf62f4e5009240a4bcb4412d0ba9
C24420082 <c24420082@mytudublin.ie>
2024-10-21 15:26:13 +0100

    week6complete

--- the-course/player.gd
+++ the-course/player.gd
@@ -22,24 +22,23 @@ func print_stuff():
 func _physics_process(delta: float) -> void:
 	print_stuff()
 	
-	position = position + transform.x
-	#var r = Input.get_axis("turn_left", "turn_right")
-	#print(r)
-#
-	#var rot = deg_to_rad(r * TURN_RATE * delta)
-	#rotate(rot)
-	#
-	#var f = Input.get_axis("move_backwards", "move_forwards")
-#
-	#var vel = transform.y * f * SPEED
-	#print(transform.y)
-	#velocity = vel
-	#
-	#
-	#if Input.is_action_pressed("fire"):
-		#var b = bullet_scene.instantiate()
-		#b.global_position = bullet_spawn_point.global_position
-		#b.global_rotation = bullet_spawn_point.global_rotation
-		#get_parent().add_child(b)
+	var r = Input.get_axis("turn_left", "turn_right")
+	print(r)
+
+	var rot = deg_to_rad(r * TURN_RATE * delta)
+	rotate(rot)
+	
+	var f = Input.get_axis("move_backwards", "move_forwards")
+
+	var vel = transform.y * f * SPEED
+	print(transform.y)
+	velocity = vel
+	
+	
+	if Input.is_action_pressed("fire"):
+		var b = bullet_scene.instantiate()
+		b.global_position = bullet_spawn_point.global_position
+		b.global_rotation = bullet_spawn_point.global_rotation
+		get_parent().add_child(b)
 	
 	move_and_slide()
